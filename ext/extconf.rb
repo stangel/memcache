c0e3f20a2f2d298bda2d424c35e647f127ada2b9
Mike Stangel <mike@geni.com>
2019-04-22 13:04:45 -0700

    split pointer fix for all platforms

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -45,6 +45,8 @@ if !ENV["EXTERNAL_LIB"]
       raise "'#{cmd}' failed" unless system(cmd)
 
       Dir.chdir(BUNDLE_PATH) do
+        puts(cmd = "patch -p1 < ../libmemcached-1.0.18_pointer-fix.diff 2>&1")
+
         if (RUBY_PLATFORM =~ /darwin(\d+)/i) and $1.to_i >= 14
           puts(cmd = "patch -p1 < ../libmemcached-1.0.18_osx-fix.diff 2>&1")
           raise "'#{cmd}' failed" unless system(cmd)
