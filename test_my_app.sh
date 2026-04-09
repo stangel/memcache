3ba0c2991cb3b9bd211ce0b3164c139531a73b31
Scott Steadman <scott.steadman@geni.com>
2026-04-09 10:16:48 -0700

    update test_my_app.sh

--- test_my_app.sh
+++ test_my_app.sh
@@ -4,6 +4,10 @@
 bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
+
+# The bundler version can change between branches
+rm -f Gemfile.lock
+
 bundle install
 
 rm lib/memcache/native_server.o
